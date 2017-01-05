3199be46a46d5dc0cbd79335fa179a723b8e9acf
Anirudh Erabelly <M1037803@mindtree.com>
2017-01-05 11:33:49 +0530

    added documentation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # shippableusecase
-application is live@URL: http://shippableusecasebyanirudherabelly.azurewebsites.net/
+>application is live@URL: http://shippableusecasebyanirudherabelly.azurewebsites.net/
+>It is built using angularjs(for API call) and bootstrap(for styling).
+>Here all the time calculations are done in according with UTC format(i.e GMT) because we are considere only with the difference between times to know the status.
+>Firstly made a call to github api to get given repository details and from that extracted the total open issue count.
+>As github api gives only 30 issues per api call, used math.ceil(totalOpenIssues/30) for getting total no.of pages and made subsequent api calls to get page by page results. 
 
+Scope for Improvements
+======================
+>As per github api pull requests are also considered in issues, we can filter them seperatley.
--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -1,35 +1,35 @@
 angular.module('shippable', [])
-    .controller('MainController', ['$http', '$scope', '$log', function($http, $scope, $log) {
+    .controller('MainController', ['$http', '$scope', function($http, $scope) {
         $scope.url = "";
         //submit button invokes this function and returns issue details. 
         $scope.getRepoIssues = function() {
-            //start of getting issues(To verify whether the repo url is correct and if correct private/public)
-            $http.get("https://api.github.com/repos/" + $scope.url.slice(19))
+            //function starts to get issues
+            $http.get("https://api.github.com/repos/" + $scope.url.slice(19))//slice part gives the org and repo details if the url is correct.
                 .then(function(response) {
-                    $scope.totalIssues = response.data.open_issues_count;
-                    $scope.issues_24h = 0;
-                    $scope.issues_24h_7d = 0;
-                    $scope.issues_7d = 0;
+                    $scope.totalIssues = response.data.open_issues_count;//total no.of open issues
+                    $scope.issues_24h = 0;//total no. of open issues opened in last 24 hrs
+                    $scope.issues_24h_7d = 0;//total no. of open issues opened in more than 24 hrs and less than 7 days.
+                    $scope.issues_7d = 0;//total no. of open issues opened more than 7 days ago
                     for (var i = 1; i <= Math.ceil($scope.totalIssues / 30); i++) {
                         $http.get("https://api.github.com/repos/" + $scope.url.slice(19) + "/issues?q=is%3Aissue+is%3Aopen&page=" + i)
                             .then(function(responseIssue) {
-                                var todayInMs = Date.parse(new Date());
+                                var todayInMs = Date.parse((new Date()).toUTCString());//converting the user Time zone to UTC(GMT)
                                 var diff = 0;
                                 for (var j = 0; j < responseIssue.data.length; j++) {
-                                    var createdDateInMs = Date.parse(responseIssue.data[j].created_at);
-                                    createdDateInMs += ((5 * 60 + 30) * 60 * 1000);
+                                    var createdDateInMs = Date.parse(responseIssue.data[j].created_at);  
                                     diff = todayInMs - createdDateInMs;
-
+                                    
+                                    //86400 secs per day(took all times in MS)
                                     if (diff <= (86400 * 1000)) $scope.issues_24h++;
                                     else if (diff > (86400 * 1000) && diff <= (86400 * 1000 * 7)) $scope.issues_24h_7d++;
                                     else $scope.issues_7d++;
                                 }
                             }, function(reasonIssue) {
-                                console.log("error callback issue");
+                                console.log("error callback issue, server Issue");
                             });
                     }
                 }, function(reason) {
-                    console.log("error callback");
+                    console.log("error callback,may be URL not recognized");
                 });
         }
     }]);
